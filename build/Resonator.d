d76833852ff650126f16510a4225355eea78452a
cdelavegamartin <c.delavega.martin@protonmail.com>
2022-04-27 18:44:59 +0100

    Remove build files

--- build/Resonator.d
+++
@@ -1,5 +0,0 @@
-/root/Bela/projects/jungle-drum/build/Resonator.o: \
-  /root/Bela/projects/jungle-drum/Resonator.cpp \
-  /root/Bela/projects/jungle-drum/Resonator.h
-
-/root/Bela/projects/jungle-drum/Resonator.h:
