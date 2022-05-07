fab19a3fd3b3ecf600753ef0f8c72f2fa8a09935
cdelavegamartin <c.delavega.martin@protonmail.com>
2022-05-07 03:00:28 +0100

    Make GUI and MIDI selectable

--- render.cpp
+++ render.cpp
@@ -12,20 +12,23 @@
 JungleDrum gDrum;
 
 // Type of Control 0=GUI, 1=MIDI
-bool gController = 1;
+bool gController = 0;
+
+// Printing?
+bool gPrint = 1;
 
 ////////// GUI ////////////////
-// // Browser-based GUI to adjust parameters
-// Gui gGui;
-// GuiController gGuiController;
+// Browser-based GUI to adjust parameters
+Gui gGui;
+GuiController gGuiController;
 
-// // Period for GUI controlled demo
-// float gNoiseInterval = 2000.0;  // ms
+// Period for GUI controlled demo
+float gNoiseInterval = 2000.0;  // ms
 
-// // samples ellapsed since last trigger measurement
-// int gSampleCounter = 0;
+// samples ellapsed since last trigger measurement
+int gSampleCounter = 0;
 
-// int gGateStatus = 0;  // Env on/off
+int gGateStatus = 0;  // Env on/off
 
 /////////// MIDI ////////////////
 
@@ -33,6 +36,7 @@ Midi gMidi;
 
 const char *gMidiPort0 = "hw:1,0,0";
 
+// Controls CC1 to CC8
 enum gMidiControls {
   MRES = 1,
   SLOPE,
@@ -69,72 +73,42 @@ bool setup(BelaContext *context, void *userData) {
     gDrum.setGainDbMax(60.0);
   }
 
-  // else if (!gController) {
-  //   // Set up the GUI
-  //   gGui.setup(context->projectName);
-  //   gGuiController.setup(&gGui, "Controls");
+  else if (!gController) {
+    // Set up the GUI
+    gGui.setup(context->projectName);
+    gGuiController.setup(&gGui, "Controls");
 
-  //   // Arguments: name, default value, minimum, maximum, increment
-  //   // Create sliders for settings
+    // Arguments: name, default value, minimum, maximum, increment
+    // Create sliders for settings
 
-  //   gGuiController.addSlider("Midi Note", 33, 21, 69, 1);
+    gGuiController.addSlider("Midi Note", 33, 21, 69, 1);
 
-  //   gGuiController.addSlider("Num Partials per dim", 8, 1, 15, 1);
-  //   gGuiController.addSlider("Aspect Ratio Lx/Ly", 0.5, 0.0, 1.0, 0.01);
-  //   gGuiController.addSlider("Filterbank Resonance", 100.0, 30.0,
-  //   150.0, 1.0); gGuiController.addSlider("Partials gain Max (dB)", 50.0,
-  //   0.0, 80, 0.1); gGuiController.addSlider("Partials gain Slope
-  //   (dB/octave)", 5.0, 0.0, 10.0,
-  //                            0.1);
+    gGuiController.addSlider("Num Partials per dim", 8, 1, 15, 1);
+    gGuiController.addSlider("Aspect Ratio Lx/Ly", 0.5, 0.0, 1.0, 0.01);
+    gGuiController.addSlider("Filterbank Resonance", 100.0, 30.0, 150.0, 1.0);
+    gGuiController.addSlider("Partials gain Max (dB)", 50.0, 0.0, 80, 0.1);
+    gGuiController.addSlider("Partials gain Slope(dB / octave) ", 5.0, 0.0,
+                             10.0, 0.1);
 
-  //   gGuiController.addSlider("Input Amplitude", 0.15, 0.0, 0.5, 0.001);
-  //   gGuiController.addSlider("Input Length (ms)", 5.0, 0.0, 20, 0.05);
+    gGuiController.addSlider("Input Amplitude", 0.15, 0.0, 0.5, 0.001);
+    gGuiController.addSlider("Input Length (ms)", 5.0, 0.0, 20, 0.05);
 
-  //   gGuiController.addSlider("Pitch Glide Amplitude", 1.0, 0.0, 2.0, 0.01);
-  //   gGuiController.addSlider("Pitch Glide Sharpness", 0.5, 0.0001, 1.0,
-  //   0.001);
+    gGuiController.addSlider("Pitch Glide Amplitude", 1.0, 0.0, 2.0, 0.01);
+    gGuiController.addSlider("Pitch Glide Sharpness", 0.5, 0.0001, 1.0, 0.001);
 
-  //   gGuiController.addSlider("VCF Resonance", 0.95, 0.7, 1.0, 0.01);
-  //   gGuiController.addSlider("Attack time (ms)", 10.0, 0.0, 200.0, 0.5);
-  //   gGuiController.addSlider("Release time (ms)", 500.0, 0.0, 1000.0, 0.5);
+    gGuiController.addSlider("VCF Resonance", 0.95, 0.7, 1.0, 0.01);
+    gGuiController.addSlider("Attack time (ms)", 10.0, 0.0, 200.0, 0.5);
+    gGuiController.addSlider("Release time (ms)", 500.0, 0.0, 1000.0, 0.5);
 
-  //   gGuiController.addSlider("Note off (ms)", 400, 10, 1000, 1.0);
-  // }
+    gGuiController.addSlider("Note off (ms)", 400, 10, 1000, 1.0);
+
+    gGuiController.addSlider("Output Gain", 1.0, 0.0, 5.0, 0.1);
+  }
   rt_printf("############ Finished Setup #####################\n");
   return true;
 }
 
-// void midiNoteOn(int noteNumber, int velocity) {
-//   if (gActiveNoteCount < kMaxNotes) {
-//     gActiveNotes[gActiveNoteCount] = noteNumber;
-//     gActiveNoteCount++;
-//     gDrum.setAmplitude(map(velocity, 1, 127, 0.001, 0.05));
-//     gDrum.noteOn(noteNumber);
-//   }
-// }
-
-// void midiNoteOff(int noteNumber){
-//   bool activeNoteChanged = false;
-
-//   for(int i = gActiveNoteCount-1; i>=0;i--){
-//     if(gActiveNotes[i]==noteNumber){
-//       if(i==gActiveNoteCount-1){
-//         activeNoteChanged = true;
-//       }
-//       for(int j=i; j<gActiveNoteCount-1; j++){
-//         gActiveNotes[j] = gActiveNotes[j+1];
-//       }
-//       gActiveNoteCount--;
-//     }
-//   }
-//   if(activeNoteChanged){
-//     gDrum.noteOff();
-//   } 
-// }
-
 void render(BelaContext *context, void *userData) {
-  float gOutMax = 0.0;
-
   if (gController) {
     // MIDI Control
     // At the beginning of each callback, look for available MIDI
@@ -222,124 +196,133 @@ void render(BelaContext *context, void *userData) {
         }
       }
     }
+
+    for (unsigned int n = 0; n < context->audioFrames; n++) {
+      float out = gDrum.process();
+      // Write the output to every audio channel
+      for (unsigned int channel = 0; channel < context->audioOutChannels;
+           channel++) {
+        audioWrite(context, n, channel, out);
+      }
+    }
   }
 
-  // else if (!gController) {
-  //   // GUI Control
-  //   // Read the slider values
-  //   float midiNote = gGuiController.getSliderValue(0);
+  else if (!gController) {
+    // For Debugging
+    float gOutMax = 0.0;
+    // GUI Control
+    // Read the slider values
+    float midiNote = gGuiController.getSliderValue(0);
 
-  //   float npar = gGuiController.getSliderValue(1);
-  //   float ratio = gGuiController.getSliderValue(2);
-  //   float membraneResonance = gGuiController.getSliderValue(3);
-  //   float maxGain = gGuiController.getSliderValue(4);
-  //   float slopeGain = gGuiController.getSliderValue(5);
+    float npar = gGuiController.getSliderValue(1);
+    float ratio = gGuiController.getSliderValue(2);
+    float membraneResonance = gGuiController.getSliderValue(3);
+    float maxGain = gGuiController.getSliderValue(4);
+    float slopeGain = gGuiController.getSliderValue(5);
 
-  //   float inAmplitude = gGuiController.getSliderValue(6);
-  //   float inLength = gGuiController.getSliderValue(7);
+    float inAmplitude = gGuiController.getSliderValue(6);
+    float inLength = gGuiController.getSliderValue(7);
 
-  //   float glideFactor = gGuiController.getSliderValue(8);
-  //   float expFactor = gGuiController.getSliderValue(9);
+    float glideFactor = gGuiController.getSliderValue(8);
+    float expFactor = gGuiController.getSliderValue(9);
 
-  //   float filterResonance = gGuiController.getSliderValue(10);
-  //   float attackTimeMs = gGuiController.getSliderValue(11);
-  //   float releaseTimeMs = gGuiController.getSliderValue(12);
+    float filterResonance = gGuiController.getSliderValue(10);
+    float attackTimeMs = gGuiController.getSliderValue(11);
+    float releaseTimeMs = gGuiController.getSliderValue(12);
 
-  //   float noteOffTimeMs = gGuiController.getSliderValue(13);
-  //   // These are only applied when the note triggers, setting only stores the
-  //   // value
+    float noteOffTimeMs = gGuiController.getSliderValue(13);
 
-  //   gDrum.setNumPartialsPerDim(npar);
-  //   gDrum.setLengthRatio(ratio);
+    float outputGain = gGuiController.getSliderValue(14);
 
-  //   gDrum.setLengthMs(inLength);
+    // These are only applied when the note triggers, setting only stores the
+    // value
 
-  //   // These are fine to change at any time
+    gDrum.setNumPartialsPerDim(npar);
+    gDrum.setLengthRatio(ratio);
+    gDrum.setGainDbSlope(slopeGain);
 
-  //   gDrum.setMembraneResonance(membraneResonance);
-  //   gDrum.setGainDbMax(maxGain);
-  //   gDrum.setGainDbSlope(slopeGain);
+    gDrum.setLengthMs(inLength);
 
-  //   gDrum.setAmplitude(inAmplitude);
+    gDrum.setPitchGlideFactor(glideFactor);
+    gDrum.setExpFactor(expFactor);
 
-  //   gDrum.setFilterResonanceMax(filterResonance);
-  //   gDrum.setAttackTimeMs(attackTimeMs);
-  //   gDrum.setReleaseTimeMs(releaseTimeMs);
-  //   gDrum.setPitchGlideFactor(glideFactor);
-  //   gDrum.setExpFactor(expFactor);
-  // }
+    // These are fine to change at any time
 
-  // // rt_printf("After Gui reads \n");
+    gDrum.setMembraneResonance(membraneResonance);
+    gDrum.setGainDbMax(maxGain);
 
-  for (unsigned int n = 0; n < context->audioFrames; n++) {
-    // if (!gController) {
-    //   float timeElapsedMilliseconds =
-    //       1000 * gSampleCounter / context->audioSampleRate;
+    gDrum.setAmplitude(inAmplitude);
 
-    //   if (timeElapsedMilliseconds > gNoiseInterval) {
-    //     // Stats for hit
+    gDrum.setFilterResonanceMax(filterResonance);
+    gDrum.setAttackTimeMs(attackTimeMs);
+    gDrum.setReleaseTimeMs(releaseTimeMs);
 
-    //     rt_printf(
-    //         "################################ PARAM "
-    //         "############################## \n");
-    //     rt_printf("Midi Note: %f   \n", midiNote);
-    //     rt_printf("Lx/Ly ratio: %f   \n", ratio);
-    //     rt_printf("Amplitude In: %f   \n", inAmplitude);
-    //     rt_printf(
-    //         "################################ DRUM  "
-    //         "############################## \n");
+    // // rt_printf("After Gui reads \n");
 
-    //     std::vector<float> freqs = gDrum.getFrequenciesHz();
-    //     float fmax = *max_element(std::begin(freqs), std::end(freqs));
-    //     rt_printf("Midi note in jungleDrum: %i \n", gDrum.getMidiNote());
-    //     rt_printf("freq0: %f \n", freqs[0]);
-    //     rt_printf("freqMax: %f \n", fmax);
-    //     rt_printf("Numpar: %i \n", gDrum.getNumPartialsPerDim());
+    for (unsigned int n = 0; n < context->audioFrames; n++) {
+      if (!gController) {
+        float timeElapsedMilliseconds =
+            1000 * gSampleCounter / context->audioSampleRate;
 
-    //     rt_printf("resBank Size: %i \n", gDrum.getSize());
-    //     rt_printf("Resonances size: %i \n", gDrum.getResonances().size());
-    //     rt_printf("gains size: %i \n", gDrum.getGainsDb().size());
-    //     rt_printf("Frequencies size: %i \n", freqs.size());
-    //     rt_printf("Pitchglide: %f \n", gDrum.getPitchGlideFactor());
+        if (timeElapsedMilliseconds > gNoiseInterval) {
+          if (gPrint) {
+            // Stats for hit
+            rt_printf("Triggered \n");
+            rt_printf(
+                "################################ PARAM "
+                "############################## \n");
+            rt_printf("Midi Note: %f   \n", midiNote);
+            rt_printf("Lx/Ly ratio: %f   \n", ratio);
+            rt_printf("Amplitude In: %f   \n", inAmplitude);
+            rt_printf(
+                "################################ DRUM  "
+                "############################## \n");
 
-    //     rt_printf("Prev Max Out: %f   \n", gOutMax);
-    //     // Only applied when triggering, this should be moved inside
-    //     JungleDrum
-    //     // once it is working
-    //
+            std::vector<float> freqs = gDrum.getFrequenciesHz();
+            float fmax = *max_element(std::begin(freqs), std::end(freqs));
+            rt_printf("Midi note in jungleDrum: %i \n", gDrum.getMidiNote());
+            rt_printf("freq0: %f \n", freqs[0]);
+            rt_printf("freqMax: %f \n", fmax);
+            rt_printf("Numpar: %i \n", gDrum.getNumPartialsPerDim());
 
-    //     gOutMax = 0.0;
-    //     gGateStatus = 1;
+            rt_printf("resBank Size: %i \n", gDrum.getSize());
+            rt_printf("Resonances size: %i \n", gDrum.getResonances().size());
+            rt_printf("gains size: %i \n", gDrum.getGainsDb().size());
+            rt_printf("Frequencies size: %i \n", freqs.size());
+            rt_printf("Pitchglide: %f \n", gDrum.getPitchGlideFactor());
 
-    //     rt_printf("Triggered \n");
-    //     gDrum.noteOn(midiNote);
+            rt_printf("Prev Max Out: %f   \n", gOutMax);
+          }
+          // reset counter for trigger
+          gSampleCounter = 0;
+          gOutMax = 0.0;
+          gGateStatus = 1;
 
-    //     gSampleCounter = 0;
-    //   } else if (timeElapsedMilliseconds > noteOffTimeMs && gGateStatus) {
-    //     gDrum.noteOff();
-    //     gGateStatus = 0;
-    //     gSampleCounter++;
-    //   } else {
-    //     gSampleCounter++;
-    //   }
-    // }
-    float out = gDrum.process();
+          gDrum.noteOn(midiNote);
 
-    //   // Debug
-    if (fabs(out) > gOutMax) {
-      gOutMax = fabs(out);
+        } else if (timeElapsedMilliseconds > noteOffTimeMs && gGateStatus) {
+          //  Equivalent to Note Off in MIDI mode
+          gDrum.noteOff();
+          gGateStatus = 0;
+        }
+      }
+      // Next audio output
+      float out = gDrum.process();
+      // Increment sample counter for trigger
+      gSampleCounter++;
+
+      // Keep track of maximum output amplitude
+      if (fabs(out) > gOutMax) {
+        gOutMax = fabs(out);
+      }
+
+      // Write the output to every audio channel
+      for (unsigned int channel = 0; channel < context->audioOutChannels;
+           channel++) {
+        audioWrite(context, n, channel, outputGain * out);
+      }
     }
-
-    // Write the output to every audio channel
-    for (unsigned int channel = 0; channel < context->audioOutChannels;
-         channel++) {
-      audioWrite(context, n, channel, out);
-    }
-
-    // gScope.log(noise, out);
   }
-
-  // rt_printf("Prev Max Out: %f   \n", gOutMax);
 }
 
 void cleanup(BelaContext *context, void *userData) {}
